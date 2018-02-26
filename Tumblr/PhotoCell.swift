8dc12e7109edbf5719aa9975187994c818b64e5d
anraag <anurag.yadav@bison.howard.edu>
2018-02-25 19:35:44 -0500

    network request

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Anurag Kumar Yadav on 2/25/18.
+//  Copyright © 2018 Anurag Kumar Yadav. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
